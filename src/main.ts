0f56f382b1d3ddea9f4f67f33af4dad71cd8d7f4
Valdo <vteixeiraromao@gmail.com>
2022-11-11 14:29:59 +0100

    Removed popup code

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
@@ -71,12 +69,5 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
